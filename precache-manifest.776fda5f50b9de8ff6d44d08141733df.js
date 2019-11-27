b89cdc5181f9f4b05372dc9042c3601bcf71ae92
Darshan Datta <darshandatta.10@gmail.com>
2019-11-27 19:18:06 +0530

    Updates

---
+++ precache-manifest.776fda5f50b9de8ff6d44d08141733df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecb11bf1141aec5aa4564ecee6e0c89",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "abde9a9ec743b19a0d28",
+    "url": "/monsters-rolodex/static/css/main.c8c983fb.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "abde9a9ec743b19a0d28",
+    "url": "/monsters-rolodex/static/js/main.2e4eb362.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
